8bb09bdd9130538360281b9303846c9e88d25c94
Ujjwal <ujjwal@Ujjwals-MacBook-Air.local>
2024-07-21 00:03:00 +0530

    Setup vue

--- venues-api/resources/js/app.js
+++ venues-api/resources/js/app.js
@@ -1,1 +1,8 @@
-import './bootstrap';
+require('./bootstrap');
+
+import { createApp } from 'vue';
+import Venue_list from './components/venue_list.vue';
+
+const app = createApp({});
+app.component('venue-list', Venue_list);
+app.mount('#app');
---
+++ venues-api/resources/js/components/venue_list.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>Venues</h1>
+        <ul>
+            <li v-for="venue in venues" :key="venue.id">{{ venue.name }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            venues: []
+        }
+    },
+    mounted() {
+        axios.get('/api/venues')
+            .then(response => {
+                this.venues = response.data;
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+}
+</script>
